9d049858bb4b98ff5bbff694b17c7f6285322a53
Cole <coltondcarter21@gmail.com>
2023-11-12 15:34:24 -0600

    Added logos for category buttons

--- src/statsScreen/StatsScreen.js
+++ src/statsScreen/StatsScreen.js
@@ -1,9 +1,14 @@
+import overallLogo from '../images/AccountIcon.png';
+import vanguardLogo from '../images/vanguardSmallIconPNG.png';
+import crucibleLogo from '../images/CrucibleLogo.png';
 function StatsScreen(props) {
     return (<div className="App">
-      <ul>
+      <div class="header">
+        <ul class="nav">
         <li><a href="App.js">LEADERBOARDS</a></li>
         <li><a href="App.js">COMPARE</a></li>
       </ul>
+      </div>
       <header className="App-header">
         <div>
          <div class="compare compare_inline">
@@ -13,14 +18,29 @@ function StatsScreen(props) {
             <p>Player Name</p>
           </div>
         </div>
-        <div class="compare_categories">
-          <h3>OVERALL</h3>
+        <div class="container compare_categories">
+          <div class="blockLogo">
+            <img src={overallLogo} /> 
+          </div>
+          <div class="text">
+            <h3>OVERALL</h3>
+          </div>
         </div>
         <div class="compare_categories">
-          <h3>VANGUARD</h3>
+          <div class="blockLogo">
+            <img src={vanguardLogo} />
+          </div>
+          <div class="text">
+            <h3>VANGUARD</h3>
+          </div>
         </div>
         <div class="compare_categories">
-          <h3>CRUCIBLE</h3>
+          <div class="blockLogo">
+            <img src={crucibleLogo} />
+          </div>
+          <div class="text">
+            <h3>CRUCIBLE</h3>
+          </div>
         </div>
       </header>
     </div>
