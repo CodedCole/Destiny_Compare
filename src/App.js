9aec4b9c8ba3ac4d825789a99f3b9338f4d31b8b
Cole <coltondcarter21@gmail.com>
2023-11-12 14:14:36 -0600

    Fixed path error in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-import StatsScreen from './StatsScreen.js';
+import StatsScreen from './statsScreen/StatsScreen.js';
 
 function App() {
   const [users, setUsers] = useState([]);
