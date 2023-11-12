cb52b66b70632e6235df9e828d577b0f237ee246
Cole <coltondcarter21@gmail.com>
2023-11-12 13:57:48 -0600

    Started Stats Screen visualization

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
 import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import StatsScreen from './StatsScreen.js';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const stats = 1; 
   useEffect(() => {
     fetch('/api/users')
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <h1>Users</h1>
-      {users.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
-      </header>
-    </div>
-  );
+  if (stats == 0) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        <h1>Users</h1>
+        {users.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+        </header>
+      </div>
+    );
+  }
+  else {
+    return <StatsScreen />; 
+  }
 }
-
-/*
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-//*/
 export default App;
---
+++ src/statsScreen/StatsScreen.js
@@ -0,0 +1,30 @@
+function StatsScreen(props) {
+    return (<div className="App">
+      <ul>
+        <li><a href="App.js">LEADERBOARDS</a></li>
+        <li><a href="App.js">COMPARE</a></li>
+      </ul>
+      <header className="App-header">
+        <div>
+         <div class="compare compare_inline">
+            <h3>COMPARE</h3>
+          </div>
+          <div class="player_name compare_inline">
+            <p>Player Name</p>
+          </div>
+        </div>
+        <div class="compare_categories">
+          <h3>OVERALL</h3>
+        </div>
+        <div class="compare_categories">
+          <h3>VANGUARD</h3>
+        </div>
+        <div class="compare_categories">
+          <h3>CRUCIBLE</h3>
+        </div>
+      </header>
+    </div>
+    )
+  }
+
+export default StatsScreen; 
