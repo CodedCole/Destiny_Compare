83ce7d014c6a950260fa7b91232058dee30a344d
CodedCole <106887848+CodedCole@users.noreply.github.com>
2023-11-12 14:05:32 -0600

    Moved homeScreen.js and added basic html to it.

---
+++ src/homeScreen/homeScreen.js
@@ -0,0 +1,9 @@
+function HomeScreen() {
+    return (
+        <div>
+            <h1>DESTINY COMPARE</h1>
+            <input type="text" placeholder="Find Player..."></input>
+            <input type="text" placeholder="Compare With..."></input>
+        </div>
+    );
+}
