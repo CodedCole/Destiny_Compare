88e5738330e23d2e19258b7467598734ccdb054e
Cole <coltondcarter21@gmail.com>
2023-11-30 13:34:44 -0600

    Updated Compare Page with some donut graphs

--- src/statsScreen/StatsScreen.js
+++ src/statsScreen/StatsScreen.js
@@ -1,6 +1,8 @@
 import overallLogo from '../images/AccountIcon.png';
 import vanguardLogo from '../images/vanguardSmallIconPNG.png';
 import crucibleLogo from '../images/CrucibleLogo.png';
+import playerLogo from '../images/emblemPlaceholderIcon.png';
+import './statsScreen.css';
 function StatsScreen(props) {
     return (<div className="App">
       <header className="App-header">
@@ -9,7 +11,10 @@ function StatsScreen(props) {
             <h3>COMPARE</h3>
           </div>
           <div class="player_name compare_inline">
-            <p>Player Name</p>
+            <div class="blockLogo">
+              <img src={playerLogo} />
+            </div>
+            <p class="compare_inline">PLAYER NAME</p>
           </div>
         </div>
         <div class="container compare_categories">
@@ -35,6 +40,28 @@ function StatsScreen(props) {
           <div class="text">
             <h3>CRUCIBLE</h3>
           </div>
+        </div>
+        <div class="played_class">
+          <p class="played_class_text">Most Played Class: Hunter</p>
+          <p class="played_class_text">Last Played Class: Hunter</p>
+        </div>
+        <div class="pie_category">
+          <p class="played_class_text">Playtime By Class</p>
+          <div class="donut">
+            <p class="pie_text">491 HRS PLAYED</p>
+          </div>
+          <p class="play_by_class">Hunter: 307 hrs</p>
+          <p class="play_by_class">Warlock: 123 hrs</p>
+          <p class="play_by_class">Titan: 61 hrs</p>
+        </div>
+        <div class="pie_category">
+          <p class="played_class_text">Kills By Class</p>
+          <div class="donut2">
+            <p class="pie_text">82,321</p>
+          </div>
+          <p class="play_by_class">Hunter: 61,740</p>
+          <p class="play_by_class">Warlock: 12,349</p>
+          <p class="play_by_class">Titan: 8,232</p>
         </div>
       </header>
     </div>
