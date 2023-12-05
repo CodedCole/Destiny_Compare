3d110338cda2013e4ca1bfe956b0c5f44f8256a2
CodedCole <106887848+CodedCole@users.noreply.github.com>
2023-12-05 14:44:14 -0600

    Updated find player by bungie display name.

--- src/backend/bungieAPI.js
+++ src/backend/bungieAPI.js
@@ -2,19 +2,19 @@ export async function SearchForPlayerByBungieID(displayName) {
     const response = await fetch(
         "https://www.bungie.net/Platform/User/Search/GlobalName/0/", 
         {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-key": "78c131c1852c4ead8fa807307bbae844"
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: {
-            "displayNamePrefix": displayName
-        }
-    });
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": "78c131c1852c4ead8fa807307bbae844"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: {
+                "displayNamePrefix": displayName
+            }
+        });
     return response.json();
 }
