32820bbfcadcb999af83342492f3d669159bf3df
my-name <carlos.salas.tulla@gmail.com>
2023-12-09 15:28:36 -0600

    Changes in homescreen looking

--- src/homeScreen/homeScreen.js
+++ src/homeScreen/homeScreen.js
@@ -26,7 +26,9 @@ export default function HomeScreen() {
     return (
         <div id="welcome">
             <img src={welcomeImage} alt="" />
+            <div>
             <h1><span className="left_of_center">DESTINY</span><span className="right_of_center">COMPARE</span></h1>
+            </div>
             <div className="search_boxes">
                 <input className="player_search" type="text" placeholder="Find Player..."></input>
                 <input className="player_search" type="text" placeholder="Compare With..."></input>
