4e3e028988916bf69b4ebbe800cbc4cb00f11322
Hayden Dewey <hayden@deweys.net>
2023-12-08 22:03:26 -0600

    Testing button and scores/kills/playtime

--- src/backend/bungieAPI.js
+++ src/backend/bungieAPI.js
@@ -45,6 +45,64 @@ async function POSTRequest(url, body) {
     return response.json();
 }
 
+// Retrieve metrics definition from bungie.net
+async function GetMetricsDefinition(manifest, language) {
+    const metricURL = `https://www.bungie.net${manifest.Response.jsonWorldComponentContentPaths[language].DestinyMetricDefinition}`;
+    const metricDefinition = await fetch(metricURL)
+        .then((result) => {
+            if (!result.ok) {
+                throw new Error("HTTP error " + result.status);
+            }
+            return result.json();
+        })
+        .catch(() => {
+            console.log("Failed a fetch");
+            return null;
+        });
+    return metricDefinition;
+}
+
+// Retrieve the manifest for metrics definitions and return a decoded name to hash object
+async function GetMetricsNameToHashTable(metricDefinition) {
+    var metricNameToHash = {};
+
+    //decode hashes to meaningful stat names
+    for (let hash in metricDefinition)
+    {
+        var propertyName = metricDefinition[`${hash}`].displayProperties.name;
+        if (metricDefinition[`${hash}`].displayProperties.description.includes("Gambit"))
+            propertyName = "Gambit " + propertyName;
+        if (metricDefinition[`${hash}`].displayProperties.description.includes("Crucible"))
+            propertyName = "Crucible " + propertyName;
+        if (metricDefinition[`${hash}`].displayProperties.description.includes("Trials"))
+            propertyName = "Trials " + propertyName;
+        if (metricDefinition[`${hash}`].displayProperties.description.includes("week"))
+            propertyName += " (Weekly)";
+        if (metricDefinition[`${hash}`].displayProperties.description.includes("this Season"))
+            propertyName += " (Season)";
+        
+        if (metricNameToHash.hasOwnProperty(propertyName))
+        {
+            //console.warn(`duplicate names: ${propertyName}\n${hash} - ${metricDefinition[`${hash}`].displayProperties.description}\n\n${metricNameToHash[`${propertyName}`]} - ${metricDefinition[`${metricNameToHash[`${propertyName}`]}`].displayProperties.description}`);
+            propertyName += `${metricDefinition[`${hash}`].index}`;
+        }
+        metricNameToHash[`${propertyName}`] = hash;
+    }
+    return metricNameToHash;
+}
+
+function CreateStatObject(metricDefinition, metrics, hash) {
+    if (!metrics.hasOwnProperty(hash))
+        return { "value": null };
+    return {
+        "value": metrics[hash].objectiveProgress.progress,
+        "name": metricDefinition[hash].displayProperties.name,
+        "description": metricDefinition[hash].displayProperties.description,
+        "lowerIsBetter": metricDefinition[hash].lowerIsBetter,
+        "icon": metricDefinition[hash].displayProperties.icon,
+    }
+}
+
 // Searches Bungie.net for users whose display name is 'displayName'.
 // Returns all matches as a json object
 export async function SearchForPlayerByBungieID(displayName) {
@@ -87,62 +145,106 @@ export async function SearchForPlayerByBungieID(displayName) {
 
 // Gets profile information from a destiny membership ID (characters, reputation, etc.)
 export async function GetProfileFromDestinyMembershipID(membershipType, membershipID) {
-    const response = await GETRequest(`https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipID}/?components=100,200,1100`);
-
-    const manifest = await GETRequest("https://www.bungie.net/Platform/Destiny2/Manifest");
-    console.log(manifest);
-    const metricURL =`https://www.bungie.net${manifest.Response.jsonWorldComponentContentPaths.en.DestinyMetricDefinition}`;
-    console.log(metricURL);
-    const metricDefinition = await fetch(metricURL)
-        .then((result) => {
-            if (!result.ok) {
-                throw new Error("HTTP error " + result.status);
-            }
-            return result.json();
-        })
-        .catch(() => {
-            console.log("Failed a fetch");
-            return null;
-        });
-    console.log(metricDefinition);
-
-    var metricNameToHash = {};
-
-    for (let hash in metricDefinition)
-    {
-        var propertyName = metricDefinition[`${hash}`].displayProperties.name;
-        if (metricDefinition[`${hash}`].displayProperties.description.includes("Gambit"))
-            propertyName = "Gambit " + propertyName;
-        if (metricDefinition[`${hash}`].displayProperties.description.includes("Crucible"))
-            propertyName = "Crucible " + propertyName;
-        if (metricDefinition[`${hash}`].displayProperties.description.includes("Trials"))
-            propertyName = "Trials " + propertyName;
-        if (metricDefinition[`${hash}`].displayProperties.description.includes("week"))
-            propertyName += " (Weekly)";
-        if (metricDefinition[`${hash}`].displayProperties.description.includes("this Season"))
-            propertyName += " (Season)";
-        
-        if (metricNameToHash.hasOwnProperty(propertyName))
-        {
-            console.warn(`duplicate names: ${propertyName}\n${hash} - ${metricDefinition[`${hash}`].displayProperties.description}\n\n${metricNameToHash[`${propertyName}`]} - ${metricDefinition[`${metricNameToHash[`${propertyName}`]}`].displayProperties.description}`);
-            propertyName += `${metricDefinition[`${hash}`].index}`;
-        }
-        metricNameToHash[`${propertyName}`] = hash;
-    }
-    console.log(metricNameToHash);
+    const response = await GETRequest(`https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipID}/?components=100,200,900,1100`);
     if (response.ErrorStatus !== "Success")
     {
         console.error(response);
         return null;
     }
+    console.log(response);
+
+    const manifest = await GETRequest("https://www.bungie.net/Platform/Destiny2/Manifest");
+    const metricDefinition = await GetMetricsDefinition(manifest, "en");
+    console.log(metricDefinition);
+    const metricNameToHash = await GetMetricsNameToHashTable(metricDefinition);
+    console.log(metricNameToHash);
+
+    let playtime = [0, 0, 0];       // titan: 0,    hunter: 1   warlock: 2
+    let recentlyPlayedClass = 0;
+    let recentlyPlayedDate = undefined;
+    for (var character in response.Response.characters.data) {
+        playtime[response.Response.characters.data[character].classType] += Number(response.Response.characters.data[character].minutesPlayedTotal);
+
+        let lastPlayedDate = Date(response.Response.characters.data[character].dateLastPlayed);
+        if (recentlyPlayedDate === undefined || lastPlayedDate > recentlyPlayedDate) {
+            recentlyPlayedDate = lastPlayedDate;
+            recentlyPlayedClass = response.Response.characters.data[character].classType
+        }
+    }
 
     var simplified = {
-        "crucibleKillsOverall": {
-            "value": response.Response.metrics.data.metrics[metricNameToHash["Crucible Opponents Defeated"]].objectiveProgress.progress,
-            "name": metricDefinition[metricNameToHash["Crucible Opponents Defeated"]].displayProperties.name,
-            "description": metricDefinition[metricNameToHash["Crucible Opponents Defeated"]].displayProperties.description
-        }
+        // playtime in minutes
+        "lastClassPlayed": (recentlyPlayedClass === 0 ? "titan" : (recentlyPlayedClass === 1 ? "hunter" : "warlock")),
+        "playtime": {
+            "total": playtime[0] + playtime[1] + playtime[2],
+            "titan": playtime[0],
+            "hunter": playtime[1],
+            "warlock": playtime[2],
+        },
+        // kills
+        "kills": {
+            "totalKillsOverall": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Total Final Blows"]),
+            "weapons": {
+                "autoRifleKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Auto Rifle Final Blows"]),
+                "bowKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Bow Final Blows"]),
+                "fusionRifleKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Fusion Rifle Final Blows"]),
+                "grenadeLauncherKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Grenade Launcher Final Blows"]),
+                "handCannonKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Hand Cannon Final Blows"]),
+                "heavyMachineGunKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Heavy Machine Gun Final Blows"]),
+                "linearFusionRifleKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Linear Fusion Rifle Final Blows"]),
+                "pulseRifleKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Pulse Rifle Final Blows"]),
+                "rocketLauncherKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Rocket Launcher Final Blows"]),
+                "scoutRifleKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Scout Rifle Final Blows"]),
+                "shotgunKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Shotgun Final Blows"]),
+                "sidearmKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Sidearm Final Blows"]),
+                "sniperRifleKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Sniper Rifle Final Blows"]),
+                "snowballKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Snowball Final Blows"]),
+                "submachineGunKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Submachine Gun Final Blows"]),
+                "swordKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Sword Final Blows"]),
+                "traceRifleKills": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Trace Rifle Final Blows"]),
+            },
+            "crucibleKillsOverall": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Crucible Opponents Defeated"]),
+            "gambitKillsOverall": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Gambit Invaders Defeated"]),
+            "pvpKillsOverall": {
+                "value": response.Response.metrics.data.metrics[metricNameToHash["Crucible Opponents Defeated"]].objectiveProgress.progress +
+                    response.Response.metrics.data.metrics[metricNameToHash["Crimson Days Opponents Defeated"]].objectiveProgress.progress +
+                    response.Response.metrics.data.metrics[metricNameToHash["Iron Burden Opponents Defeated"]].objectiveProgress.progress +
+                    response.Response.metrics.data.metrics[metricNameToHash["Trials Opponents defeated"]].objectiveProgress.progress +
+                    response.Response.metrics.data.metrics[metricNameToHash["Gambit Invaders Defeated"]].objectiveProgress.progress,
+                "name": "PvP Kills",
+                "description": "Players defeated over account lifetime."
+            },
+        },
+        "strikes": {
+            "totalStrikes": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Completions"]),
+            "nightfalls": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall Challenge Completions"]),
+            "nightfallScores": {
+                "armsDealer": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Arms Dealer\" Score"]),
+                "birthplaceOfTheVile": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Birthplace of the Vile\" Score"]),
+                "broodhold": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Broodhold\" Score"]),
+                "corrupted": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Corrupted\" Score"]),
+                "devilsLair": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Devils' Lair\" Score"]),
+                "disgraced": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Disgraced\" Score"]),
+                "exodusCrash": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Exodus Crash\" Score"]),
+                "fallenSaber": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Fallen S.A.B.E.R.\" Score"]),
+                "festeringCore": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Festering Core\" Score"]),
+                "gardenWorld": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"A Garden World\" Score"]),
+                "glassway": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Glassway\" Score"]),
+                "hollowedLair": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Hollowed Lair\" Score"]),
+                "insightTerminus": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Insight Terminus\" Score"]),
+                "invertedSpire": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Inverted Spire\" Score"]),
+                "lakeOfShadows": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Lake of Shadows\" Score"]),
+                "lightblade": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Lightblade\" Score"]),
+                "provingGrounds": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Proving Grounds\" Score"]),
+                "pyramidion": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Pyramidion\" Score"]),
+                "savathunsSong": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Savathûn's Song\" Score"]),
+                "scarletKeep": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"The Scarlet Keep\" Score"]),
+                "strangeTerrain": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Strange Terrain\" Score"]),
+                "treeOfProbabilities": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Tree of Probabilities\" Score"]),
+                "wardenOfNothing": CreateStatObject(metricDefinition, response.Response.metrics.data.metrics, metricNameToHash["Nightfall: \"Warden of Nothing\" Score"]),
+            }
+        },
     };
-    console.log(response);
+    
     return simplified;
 }
--- src/homeScreen/homeScreen.js
+++ src/homeScreen/homeScreen.js
@@ -2,13 +2,24 @@ import "./homeScreen.css"
 import welcomeImage from "../images/destiny-ghost.jpg"
 import { SearchForPlayerByBungieID, GetProfileFromDestinyMembershipID } from "../backend/bungieAPI.js";
 
-export default function HomeScreen() {
+function SearchPlayer(){
     SearchForPlayerByBungieID("CodedCole").then((data) => {
         console.log(data);
         GetProfileFromDestinyMembershipID(data.searchResults[0].PrimaryDestinyMembershipType, data.searchResults[0].PrimaryDestinyMembershipID).then((data) => {
             console.log(data);
         });
     });
+    SearchForPlayerByBungieID("DJ Tears").then((data) => {
+        console.log(data);
+    });
+}
+
+// Test Players
+// CodedCole#5868
+// DJ Tears#4567
+
+export default function HomeScreen() {
+    
     return (
         <div id="welcome">
             <img src={welcomeImage} alt="" />
@@ -17,6 +28,7 @@ export default function HomeScreen() {
                 <input className="player_search" type="text" placeholder="Find Player..."></input>
                 <input className="player_search" type="text" placeholder="Compare With..."></input>
             </div>
+            <button onClick={SearchPlayer}>Compare</button>
         </div>
     );
 }
